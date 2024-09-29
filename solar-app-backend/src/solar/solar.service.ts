4b48fce5cfd0766b7d662f1999fb617c493c1613
Sol <stoledo7@hotmail.com>
2024-09-29 18:49:22 -0300

    comenzar a refactorizar

--- solar-app-backend/src/solar/solar.service.ts
+++ solar-app-backend/src/solar/solar.service.ts
@@ -11,36 +11,6 @@ import { YearlyAnualConfigurations } from 'src/interfaces/yearly-anual-configura
 export class SolarService {
   constructor(private readonly calculadoraService: CalculadoraService) {}
 
-  /* async getSolarData(latitude: number, longitude: number): Promise<any> {
-    if (isNaN(latitude) || isNaN(longitude)) {
-      throw new HttpException(
-        'Invalid coordinates received',
-        HttpStatus.BAD_REQUEST,
-      );
-    }
-
-    const apiKey = process.env.GOOGLE_API_KEY;
-    const url = `https://solar.googleapis.com/v1/buildingInsights:findClosest?location.latitude=${latitude}&location.longitude=${longitude}&key=${apiKey}`;
-
-    try {
-      const response = await axios.get(url);
-      return response.data;
-    } catch (error) {
-      if (error.response && error.response.status === 404) {
-        throw new HttpException(
-          'Location out of coverage',
-          HttpStatus.BAD_REQUEST,
-        );
-      } else {
-        console.error('Error fetching data from API:', error.message);
-        throw new HttpException(
-          `An error occurred while fetching data: ${error.message}`,
-          HttpStatus.INTERNAL_SERVER_ERROR,
-        );
-      }
-    }
-  } */
-
   async getSolarData(latitude: number, longitude: number): Promise<any> {
     // Verifica si las coordenadas son válidas
     if (isNaN(latitude) || isNaN(longitude)) {
