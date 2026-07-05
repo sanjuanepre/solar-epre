4e61a5873563322996e08dec9f144f4d52ee45dd
semper <spereyra.jus@gmail.com>
2026-07-05 20:43:02 -0300

    fix(frontend): remove environments from gitignore and track them

---
+++ solar-app-frontend/src/environments/environments.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  googleMapsApiKey: 'AIzaSyDGg1OqQAEkdFuAGuulVTSdg05axXeUb94'
+};
---
+++ solar-app-frontend/src/environments/environments.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  googleMapsApiKey: 'AIzaSyDGg1OqQAEkdFuAGuulVTSdg05axXeUb94'
+};
