0dca9b522dcdceed7a2b3a10fc4e7b95fb1caaa8
semper <spereyra.jus@gmail.com>
2026-07-10 19:13:58 -0300

    fix: proteccion contra toFixed() nulo en tarifa-intercambio.component.ts durante inicializacion asincrona

--- solar-app-frontend/src/app/pasos/paso3/tarifa-intercambio/tarifa-intercambio.component.ts
+++ solar-app-frontend/src/app/pasos/paso3/tarifa-intercambio/tarifa-intercambio.component.ts
@@ -1,4 +1,5 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { SharedService } from 'src/app/services/shared.service';
 
 @Component({
@@ -6,18 +7,27 @@ import { SharedService } from 'src/app/services/shared.service';
   templateUrl: './tarifa-intercambio.component.html',
   styleUrls: ['./tarifa-intercambio.component.css'],
 })
-export class TarifaIntercambioComponent implements OnInit, AfterViewInit {
-  tarifaIntercambioUsdkWh!: number;
+export class TarifaIntercambioComponent implements OnInit, AfterViewInit, OnDestroy {
+  tarifaIntercambioUsdkWh: number = 0;
+  private sub?: Subscription;
 
   constructor(private sharedService: SharedService) {}
 
   ngOnInit(): void {
-    setTimeout(() => {
-      this.tarifaIntercambioUsdkWh = parseFloat(
-        this.sharedService.getTarifaIntercambioUsdkWh().toFixed(3)
-      );
-    }, 300);
+    this.sub = this.sharedService.tarifaIntercambioUsdkWh$.subscribe((val) => {
+      if (val !== null && val !== undefined && !isNaN(val)) {
+        this.tarifaIntercambioUsdkWh = parseFloat(val.toFixed(3));
+      } else {
+        this.tarifaIntercambioUsdkWh = 0;
+      }
+    });
   }
 
   ngAfterViewInit(): void {}
+
+  ngOnDestroy(): void {
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
+  }
 }
