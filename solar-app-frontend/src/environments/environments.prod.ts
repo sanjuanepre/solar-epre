b4eb8cca93251eed3cd872f750fdfb9be23bbcb6
semper <spereyra.jus@gmail.com>
2026-08-09 11:01:48 -0300

    chore: use permanent git-branch alias for backend URL (no more URL chasing)

--- solar-app-frontend/src/environments/environments.prod.ts
+++ solar-app-frontend/src/environments/environments.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   googleMapsApiKey: 'AIzaSyAUMiT8cjb-FwGddpPuIK8D3gZJjUphIY0',
-  apiUrl: 'https://solar-app-backend-84k2losyg-epres-projects-7c2ad83e.vercel.app'
+  apiUrl: 'https://solar-app-backend-git-feature-me-addd07-epres-projects-7c2ad83e.vercel.app'
 };
