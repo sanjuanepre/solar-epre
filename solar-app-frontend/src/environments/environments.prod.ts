02b0dfef0ddb93a2c65e520ce507bc91a6a61be7
semper <spereyra.jus@gmail.com>
2026-08-25 19:02:25 -0300

    fix(env): revert apiUrl to feature/mejora-selector-area backend which has all required endpoints

--- solar-app-frontend/src/environments/environments.prod.ts
+++ solar-app-frontend/src/environments/environments.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   googleMapsApiKey: 'AIzaSyAUMiT8cjb-FwGddpPuIK8D3gZJjUphIY0',
-  apiUrl: 'https://solar-app-backend-khaki.vercel.app'
+  apiUrl: 'https://solar-app-backend-git-feature-me-addd07-epres-projects-7c2ad83e.vercel.app'
 };
